78299069d789ae0179c3a864f29b58b248f63d37
Jacinto Rodrigues <jacinto_rodrigues1@hotmail.com>
2018-08-21 23:54:01 +0100

    add initial webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,36 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
+  template: path.resolve(__dirname, 'src/index.html'),
+  filename: './index.html',
 });
 
 module.exports = {
+  entry: {
+    vendor: ['react', 'react-dom', 'react-router-dom'],
+    context: path.resolve(__dirname, 'src'),
+    app: [
+      path.resolve(__dirname, 'src/index'),
+    ],
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
-  plugins: [htmlPlugin]
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [htmlPlugin],
 };
