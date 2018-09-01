a7d67e8ee44dc2cf2559a90525f4bc9c4b2a9aff
Jacinto Rodrigues <jacinto_rodrigues1@hotmail.com>
2018-09-01 18:51:54 +0100

    Add root container

---
+++ src/containers/Root/Root.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import routes from '../../Routes';
+
+const Root = ({ store, history }) => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        {routes}
+      </div>
+    </ConnectedRouter>
+  </Provider>
+);
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
+};
+
+export default Root;
