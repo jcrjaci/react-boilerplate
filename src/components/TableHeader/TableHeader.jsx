f4691c95e5f0b5250972d9553b21d8303aaba21e
Jacinto Rodrigues <jacinto_rodrigues1@hotmail.com>
2018-09-01 18:52:32 +0100

    Add table header component

---
+++ src/components/TableHeader/TableHeader.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './TableHeader.scss';
+
+const TableHeader = ({ headers }) => (
+  <div className="table-header">
+    {headers.map(field => <div key={field}>{field}</div>)}
+  </div>
+);
+
+TableHeader.propTypes = {
+  headers: PropTypes.arrayOf(PropTypes.string),
+};
+
+TableHeader.defaultProps = {
+  headers: [],
+};
+
+export default TableHeader;
