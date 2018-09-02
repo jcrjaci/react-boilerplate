475430e594ea01afcb8f221f33ac1eef2b26a75a
Jacinto Rodrigues <jacinto_rodrigues1@hotmail.com>
2018-09-03 00:48:17 +0100

    Add children to table component.

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -3,21 +3,24 @@ import PropTypes from 'prop-types';
 import TableHeader from '../TableHeader/TableHeader';
 import TableBody from '../TableBody/TableBody';
 
-const Table = ({ data, headers }) => (
+const Table = ({ data, headers, children }) => (
   <div className="table-container">
     <TableHeader headers={headers} />
     <TableBody data={data} />
+    {children}
   </div>
 );
 
 Table.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object),
   headers: PropTypes.arrayOf(PropTypes.string),
+  children: PropTypes.node,
 };
 
 Table.defaultProps = {
   data: [],
   headers: [],
+  children: null,
 };
 
 export default Table;
