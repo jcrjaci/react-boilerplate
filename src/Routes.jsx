5624ecd89f114180317cd071de63315244b6680e
Jacinto Rodrigues <jacinto_rodrigues1@hotmail.com>
2018-09-03 00:49:00 +0100

    Add pagination route.

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch, BrowserRouter } from 'react-router-dom';
 import Home from './containers/Home/Home';
 import NotFound from './containers/NotFound/NotFound';
 
@@ -7,6 +7,7 @@ export default (
   <HashRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/coins/:page([0-9]*)" component={Home} />
       <Route component={NotFound} />
     </Switch>
   </HashRouter>
