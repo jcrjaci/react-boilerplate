9e77cd6837cddb9fbd73717fdf743fb1e99bc144
Jacinto Rodrigues <jacinto_rodrigues1@hotmail.com>
2018-09-07 14:43:39 +0100

    Add enzyme

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  // setupTestFrameworkScriptFile: '<rootDir>src/setupTests.js',
   moduleNameMapper: {
     '^.+\\.(css|less|scss)$': 'babel-jest',
   },
