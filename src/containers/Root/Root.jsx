11363f11769ea210e7c6bc4c082747cbde92d4fa
Jacinto Rodrigues <jacinto_rodrigues1@hotmail.com>
2018-09-11 22:40:48 +0100

    fix root component proptypes

--- src/containers/Root/Root.jsx
+++ src/containers/Root/Root.jsx
@@ -4,6 +4,11 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import routes from '../../Routes';
 
+/** Root component
+* @param {object} props Component props
+* @param {array} store Redux store.
+* @param {array} history Route history.
+*/
 const Root = ({ store, history }) => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
@@ -15,8 +20,13 @@ const Root = ({ store, history }) => (
 );
 
 Root.propTypes = {
-  store: PropTypes.objectOf(PropTypes.object).isRequired,
-  history: PropTypes.objectOf(PropTypes.object).isRequired,
+  store: PropTypes.shape({
+    dispatch: PropTypes.func.isRequired,
+    getState: PropTypes.func.isRequired,
+    replaceReducer: PropTypes.func.isRequired,
+    subscribe: PropTypes.func.isRequired,
+  }).isRequired,
+  history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
 };
 
 export default Root;
