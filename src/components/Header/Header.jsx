279f72525b6a2f9bc7fd126cb2c894db30ace4d1
Jacinto Rodrigues <jacinto_rodrigues1@hotmail.com>
2018-09-14 23:00:43 +0100

    Add jsdoc to Header component.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Header.scss';
 
+/** Header component
+* @param {object} props Component props
+* @param {String} props.title Title to display in header.
+*/
 const Header = ({ title }) => <header className="title">{title}</header>;
 
 Header.propTypes = {
