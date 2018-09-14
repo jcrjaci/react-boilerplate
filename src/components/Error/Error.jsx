52bb793325e6557087414366709832ce4ef7dd8e
Jacinto Rodrigues <jacinto_rodrigues1@hotmail.com>
2018-09-14 22:59:51 +0100

    Add jsdoc to error component.

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Error.scss';
 
+/** Error component
+* @param {object} props Component props
+* @param {Boolean} props.msg Error message to display
+*/
 const Error = ({ msg }) => (
   <div className="error-msg">
     <span>{msg || 'An error occurred'}</span>
