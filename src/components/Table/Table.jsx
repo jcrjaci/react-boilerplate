e75d4142f3429957cc5e608d847b2c984ef54108
Jacinto Rodrigues <jacinto_rodrigues1@hotmail.com>
2018-09-16 22:02:49 +0100

    Add jsdoc to table component.

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -3,6 +3,12 @@ import PropTypes from 'prop-types';
 import TableHeader from '../TableHeader/TableHeader';
 import TableBody from '../TableBody/TableBody';
 
+/** Table component
+* @param {object} props Component props
+* @param {array} props.data list of data to display in table body.
+* @param {array} props.headers List of headers to be displayed in table header.
+* @param {HTMLElement} props.children Children node to display after the table body.
+*/
 const Table = ({ data, headers, children }) => (
   <div className="table-container">
     <TableHeader headers={headers} />
