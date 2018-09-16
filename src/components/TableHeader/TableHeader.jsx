b3802cd421405ce5473f03ee40c7c7a3fbf17a05
Jacinto Rodrigues <jacinto_rodrigues1@hotmail.com>
2018-09-16 22:05:28 +0100

    Add jsdoc to table header component.

--- src/components/TableHeader/TableHeader.jsx
+++ src/components/TableHeader/TableHeader.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './TableHeader.scss';
 
+/** Table component
+* @param {object} props Component props
+* @param {array} props.headers List of headers to be displayed in table header.
+*/
 const TableHeader = ({ headers }) => (
   <div className="table-header">
     {headers.map(field => <div key={field}>{field}</div>)}
