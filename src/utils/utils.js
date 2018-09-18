a73cbd9bc97c5d76597c09e3d32f122f3bf93611
Jacinto Rodrigues <jacinto_rodrigues1@hotmail.com>
2018-09-18 22:09:46 +0100

    Add jsdoc to utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,15 +1,30 @@
+/**
+ * validate if the parameter is a valid number.
+ * @param  {any} number
+ * @return {Boolean} true if the parameter is a valid number or false if not.
+ */
 export function isNumber(number) {
   const numberConverted = Number(number);
 
   return !Number.isNaN(numberConverted) && Number.isFinite(numberConverted);
 }
 
+/**
+ * Validate if the parameter is a valid positive number.
+ * @param  {any} number
+ * @return {Boolean} true if the parameter is a valid positive number.
+ */
 export function numberIsPositive(number) {
   const numberConverted = Number(number);
 
   return isNumber(number) && numberConverted > 0;
 }
 
+/**
+ * Validate if the parameter is a valid number and if is zero or greater.
+ * @param  {any} number
+ * @return {Boolean} true if the parameter is a valid number and if is zero or greater.
+ */
 export function numberIsZeroOrGreater(number) {
   const numberConverted = Number(number);
 
