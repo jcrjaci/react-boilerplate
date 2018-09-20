a78c50d3b1262507d8411de9b3182dcbac249e03
Jacinto Rodrigues <jacinto_rodrigues1@hotmail.com>
2018-09-20 21:58:28 +0100

    Add jsdoc to not found component.

--- src/containers/NotFound/NotFound.jsx
+++ src/containers/NotFound/NotFound.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './NotFound.scss';
 
-
+/** Not Found container
+ * Dispay a Not found page in case of an invalid route.
+*/
 const NotFound = () => (
   <div className="not-found">
     <div className="message">
